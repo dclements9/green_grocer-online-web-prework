5e599247d3e83e3a150510fa76efa6895c1b35ac
Dylan Clements <dclements@protonmail.com>
2019-01-13 13:12:11 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,60 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  output_hash = {}
+    item_array = []
+      cart.each do |object|
+        object.each do |item, description|
+          item_array << item
+          output_hash[item] = description
+          output_hash[item][:count] = item_array.count(item)
+      end
+    end
+  output_hash
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+coupon_cart = cart
+coupons.each do |savings|
+      savings_item = savings[:item]
+    if coupon_cart.has_key?(savings_item)
+        item_count = 0
+        while coupon_cart[savings[:item]][:count] >= savings[:num]
+            item_count += 1
+            coupon_cart["#{savings[:item]} W/COUPON"] = {
+              price: savings[:cost],
+              clearance: coupon_cart[savings[:item]][:clearance],
+              count: item_count}
+            coupon_cart[savings[:item]][:count] -= savings[:num]
+        end
+      end
+    end
+  coupon_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  output_cart = cart
+    cart.each do |item, description|
+      if cart[item][:clearance] == true
+          output_price = cart[item][:price] * 0.8
+          output_cart[item][:price] = output_price.round(2)
+    end
+  end
+ output_cart
 end
 
 def checkout(cart, coupons)
-  # code here
+    total = 0
+  merged_cart = consolidate_cart(cart)
+  savings_cart = apply_coupons(merged_cart, coupons)
+  output_cart = apply_clearance(savings_cart)
+  
+  output_cart.each do |item, description|
+    total += description[:price] * description[:count]
+  end
+    if total > 100
+      total *= 0.9
+  end
+ total
 end
